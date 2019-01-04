f460c210c465ac6cf9ab89eda10c9f9a88bb13ee
Omar Hydrah <omarhydrah@tutanota.de>
2019-01-04 23:03:56 +0200

    Change 'job' attribute to 'title'

--- Job.py
+++ Job.py
@@ -4,16 +4,18 @@ class Job:
 	company  = ""
 	salary   = ""
 	location = ""
+	title    = ""
 
 
-	def __init__(self, category, job, salary=None, company=None, url=None, location=None):
+	def __init__(self, category, title, salary=None, company=None, url=None, 
+		location=None):
 		self.category = category
-		self.job      = job
+		self.title      = title
 		self.salary   = salary
 		self.company  = company
 		self.url      = url
 		self.location = location
 
 	def __str__(self):
-		return "{category: %s, job: %s, salary: %s, company: %s, location : %s}" % (
-			self.category, self.job, self.salary, self.company, self.location)
+		return "{category: %s, title: %s, salary: %s, company: %s, location : %s}" % (
+			self.category, self.title, self.salary, self.company, self.location)
