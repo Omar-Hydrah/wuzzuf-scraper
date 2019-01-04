e309930ec6370698b7eea68977b78c0bf55409be
Omar Hydrah <omarhydrah@tutanota.de>
2019-01-04 19:46:34 +0200

    Create __str__ method in Job.py

--- Job.py
+++ Job.py
@@ -6,10 +6,14 @@ class Job:
 	location = ""
 
 
-	def __init__(self, category, job, salary=None, company=None, url=None):
+	def __init__(self, category, job, salary=None, company=None, url=None, location=None):
 		self.category = category
 		self.job      = job
 		self.salary   = salary
 		self.company  = company
 		self.url      = url
+		self.location = location
 
+	def __str__(self):
+		return "{category: %s, job: %s, salary: %s, company: %s, location : %s}" % (
+			self.category, self.job, self.salary, self.company, self.location)
