001f7535c9ebedf493d513e7aacda41183ba3c4b
Omar Hydrah <omarhydrah@tutanota.de>
2019-01-04 23:02:30 +0200

    Add getJobs() to Sql.py and organize some lines

--- Sql.py
+++ Sql.py
@@ -1,5 +1,5 @@
 import sqlite3 as sqlite
-import Job
+from Job import Job
 
 class Sql():
 	database   = ""
@@ -19,19 +19,30 @@ class Sql():
 	def createTable(self):
 		query = """create table if not exists jobs
 			(id integer primary key autoincrement, 
-			category text, job text, salary text, company text, 
+			category text, title text, salary text, company text, 
 			location text, url text)"""
 		self.cursor.execute(query)
 		self.connection.commit()
 
 	def insertJob(self, job):
-		query = """insert into jobs(category, job, salary, company, url, location) 
+		query = """insert into jobs(category, title, salary, company, url, location) 
 			values(?, ?, ?, ?, ?, ?)"""
-		self.cursor.execute(query, (job.category, job.job, job.salary, 
+		self.cursor.execute(query, (job.category, job.title, job.salary, 
 			job.company, job.url, job.location))
 		self.connection.commit()
 
+	def getJobs(self):
+		query = "select * from jobs"
+		self.cursor.execute(query)
+		jobResults = self.cursor.fetchall()
+		jobs = []
 
+		for result in jobResults:
+			job = Job(result[1], result[2], result[3], 
+				result[4],  result[6], result[5])
+			jobs.append(job)
+
+		return jobs
 
 	def close(self):
 		self.cursor.close()
