b8ca2e63461f8abf05c9fa0a7e945cc42ad5bf34
Omar Hydrah <omarhydrah@tutanota.de>
2019-01-04 23:08:23 +0200

    Allow to pass args to download or write jobs

--- wuzzuf_scraper.py
+++ wuzzuf_scraper.py
@@ -8,28 +8,45 @@ from bs4 import BeautifulSoup as Soup
 from Job import Job
 from Sql import Sql
 from Scraper import Scraper
+from HtmlCreator import HtmlCreator
 
+def downloadJobs(keyword, sql):
+	scraper = Scraper(keyword)
+	scraper.sql = sql
+	print("Jobs count: " +  str(scraper.jobCount))
+	scraper.searchForKeyword()
+
+	print("Finished downloading")
+
+def writeHtmlFile(keyword, sql):
+	jobs = sql.getJobs()
+	# print(len(jobs))
+	# print(jobs[0])
+	# print(jobs[1])
+	html = HtmlCreator(keyword, jobs)
+	print("File written")
 
 def main(argv):
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-k", "--keyword", help="The keyword to search for on wuzzuf")
+	parser.add_argument("-a", "--action", help="Action <download | write>")
 
 	args = parser.parse_args()
 
-	keyword = args.keyword  
+	keyword = args.keyword
+	action  = args.action
 
 	sql = Sql(keyword)
 	sql.open()
 	sql.createTable()
 
-	scraper = Scraper(keyword)
-	scraper.sql = sql
-	print("Total count: " +  str(scraper.jobCount))
-	scraper.searchForKeyword()
-
-	print("Finished downloading")
-
+	if action == "download":
+		downloadJobs(keyword, sql)
+	elif action == "write":
+		writeHtmlFile(keyword, sql) 
+	else:
+		print("Missing --action to jobs; to download or write them to html")
 
 
 if __name__ == "__main__":
